105b8e9e1df363ba130dcd33178965bc50c9d05c
Amandine95 <yangguanglg@163.com>
2018-10-29 19:36:00 +0800

    类视图添加装饰器,装在方法上,重写dispatch方法,实现给所有方法添加装饰，也可以给单个方法添加

--- classviews/views.py
+++ classviews/views.py
@@ -3,6 +3,7 @@
 """
 from django.http import HttpResponse
 from django.views.generic import View
+from django.utils.decorators import method_decorator
 
 
 # 定义一个装饰器(闭包)
@@ -14,7 +15,6 @@ def my_decoration(view_func):
         # 视图函数返回结果要进行返回
         return view_func(request, *args, **kwargs)
 
-
     return wrapper
 
 
@@ -22,11 +22,20 @@ def my_decoration(view_func):
 # @my_decoration  <=> view = my_decoration(view)
 # def view(request):
 #     return HttpResponse('view')
+"""
+类视图/类视图方法添加装饰器都要 method_decorator
+"""
 
 
 class DemoView(View):
     """自定义视图类"""
 
+    @method_decorator(my_decoration)
+    def dispatch(self, request, *args, **kwargs):
+        """重写父类方法,所有请求方式都会调用这个方法"""
+        return super().dispatch(request, *args, **kwargs)
+
+    # @method_decorator(my_decoration) 给单独的方法添加装饰器
     def get(self, request):
         """get方法"""
         return HttpResponse('get')
