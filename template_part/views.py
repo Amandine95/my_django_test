d3a4348da4469ad7c091f34c3bfd509ec66bc82d
Amandine95 <yangguanglg@163.com>
2018-10-31 20:14:32 +0800

    django模板的完整写法

--- template_part/views.py
+++ template_part/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views import View
+from django.template import RequestContext
+from django.template import loader
+from django.views.generic import View
 
 """
 django 的模板
@@ -11,8 +13,20 @@ class TemplateView(View):
     """类视图"""
 
     def get(self, request):
+        """简写用render方法"""
+
         # 上下文，模板中展示的数据
         context = {
             "city": "shanghai"
         }
         return render(request, 'first_html.html', context)
+
+    def post(self, request):
+        """完整写法"""
+
+        # 获取模板
+        template = loader.get_template('first_html.html')
+        # 创建上下文
+        context = {'city':'beijing'}
+        # 渲染
+        return HttpResponse(template.render(context))
